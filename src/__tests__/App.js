8619a6d3cb3e7f7c42c922bbdbd1f7a8b92f9876
attilac <attila.cederbygd@gmail.com>
2018-01-19 14:01:48 +0100

    Added some tests to App.js

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -1,7 +1,41 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, mount, shallow } from 'enzyme';
 import App from '../components/App';
 
 test('renders the app', () => {
   render(<App />);
+});
+
+it('should render <App /> with no Persona selected', () => {
+  const wrapper = shallow(<App />);
+  const user = wrapper.state().currentPersona;
+  expect(user).toBe("");
+});
+
+it('should render <App /> with Persona', () => {
+  const wrapper = shallow(<App />);
+  const persona = 'Zak';
+  wrapper.setState({'currentPersona': persona});
+  const user = wrapper.state().currentPersona;
+  expect(user).toBe("Zak");
+});
+
+it('should render <App /> with default Page', () => {
+  const wrapper = shallow(<App />);
+  const { currentPage } = wrapper.state();
+  expect(currentPage).toBe("home");
+});
+
+it('should render <App /> changed Page', () => {
+  const wrapper = shallow(<App />);
+  const page = "bot";
+  const { currentPage:defaultPage } = wrapper.state();
+  expect(defaultPage).toBe("home");
+  wrapper.setState({'currentPage': page});
+  const { currentPage } = wrapper.state();
+  expect(currentPage).toBe("bot");
+});
+
+it('should render fetch user and display on page', () => {
+
 });
