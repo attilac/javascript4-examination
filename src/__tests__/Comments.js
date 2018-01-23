90a00cf2833a93006dba2d71de02ec11d666038e
jesperengstrom <jengstro@gmail.com>
2018-01-23 14:59:01 +0100

    Comments component + comment mock function

--- src/__tests__/Comments.js
+++ src/__tests__/Comments.js
@@ -1,48 +1,31 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import Comments from '../components/Comments';
-
-// const key = "data";
-// const value = JSON.stringify(
-// [
-//   { 
-//     id: '1', 
-//     title: 'Title',
-//     content: 'Content',
-//     author: 'Esmeralda',
-//     date: (new Date()).toLocaleString()
-//   },
-// ]);
-
-const mockComment = JSON.stringify(
-  [
-    {
-      comment: 'Comment',
-      id: '1',
-      postId: 1,
-      author: 'testAuthor',
-      date: (new Date()).toLocaleString()
-    }
-  ]
-);
-
-localStorage.setItem('comments', mockComment);
-
-// just fetches []
-// it('should set comment as state on mount', ()=> {
-//   const wrapper = mount(<Comments postId="1" currentPersona="test" />)
-//   console.log(wrapper.state().comments)
-//   expect(wrapper.state().comments).toBeTruthy();
-// })
-
-// it('should fetch my mock comment', () => {
-//   const wrapper = mount(<Comments postId='1' currentPersona="test" />)
-//   wrapper.instance().setCommentsFromLocalStorage('1')
-//   console.log(wrapper.state().comments);
-//   expect(wrapper.state().comments[0].comment).toEqual('Comment')
-// });
+import postMockComment from '../setupTests';
 
 
-it('test', ()=>{
-  expect(true);
+beforeEach(() => {
+  postMockComment();
+});
+
+afterEach(() => {
+  localStorage.clear();
+});
+
+it('should fetch comment as state on mount', ()=> {
+  const wrapper = mount(<Comments postId="1" currentPersona="testAuthor" />)
+  expect(wrapper.state().comments[0].comment).toEqual('Comment')
 })
+
+it('should remove comment from state', ()=> {
+  const wrapper = mount(<Comments postId="1" currentPersona="testAuthor" />)
+  wrapper.instance().removeComment("1");
+  expect(wrapper.state().comments).toEqual([]);
+})
+
+it('should match snapshot', () => {
+  const wrapper = mount(<Comments postId="1" currentPersona="testAuthor" />)
+  expect(wrapper).toMatchSnapshot();
+})
+
+
