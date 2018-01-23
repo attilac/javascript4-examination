ece423528c86a7634ca3c1ed864cbf967769d18c
attilac <attila.cederbygd@gmail.com>
2018-01-23 14:00:39 +0100

    tests for createNewComment

--- src/__tests__/CreateNewComment.js
+++ src/__tests__/CreateNewComment.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import CreateNewComment from '../components/CreateNewComment';
 
 describe('CreateNewComment unit tests', ()=> {
 
-  it('form submission should clear state after submitting to loacalStorage', () => {
+  it('should store comment to localStorage and clear state after', () => {
     const updateComments = jest.fn();
-    const wrapper = mount(<CreateNewComment  author="Attila" postId="1" updateComments={updateComments}/>);
+    const wrapper = mount(<CreateNewComment  author="Attila" postId="1" updateComments={updateComments} />);
     wrapper.setState({comment: 'testcomment'})
     expect(wrapper.state().comment).toBe('testcomment');   
     wrapper.find('form').simulate('submit');
-    // console.log(wrapper.state());
+    // console.log( JSON.parse(localStorage.getItem('comments'))[0].comment);
+    const newComment = JSON.parse(localStorage.getItem('comments'))[0].comment;
+    expect(newComment).toBe('testcomment');
     expect(wrapper.state().comment).toBe(''); 
   });
 
   it('textfield change should update component state', () => {
     const updateComments = jest.fn();
-    const wrapper = mount(<CreateNewComment  author="Attila" postId="1" updateComments={updateComments}/>);
+    const wrapper = mount(<CreateNewComment  author="Attila" postId="1" updateComments={updateComments} />);
     wrapper.find('#comment').simulate('change', {target: { name: 'comment', value:'testcomment'}});
     expect(wrapper.state().comment).toBe('testcomment'); 
   });  
