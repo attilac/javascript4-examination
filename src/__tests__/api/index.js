04776a61624170c8fc8541895e5f95f01acbe3aa
attilac <attila.cederbygd@gmail.com>
2018-01-24 14:24:52 +0100

    api tests

--- src/__tests__/api/index.js
+++ src/__tests__/api/index.js
@@ -1,24 +1,54 @@
 import * as api from '../../api';
 
 describe('api tests', ()=> {
-  // jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-
-  it('should return random message', () => {
-    console.log('hej');
-  });
 
   jest.useFakeTimers();
 
-  it('works with resolves', () => {
+  const responses = [
+    'Hello there fellow human',
+    'Nice game last night',
+    'This weather huh!',
+    'Back on the daily grid! Work, work work!',
+    'Have you tried a new diet recently?',
+    'Did you know that Agneta divorced Göran last year?',
+    'Add me on snapchat! @totallyhuman',
+    'You can tell that I am a human because I respond slowly. A bot would respond instantly',
+    'I would like one cold beers to consume please',
+    'Kneel to your robot overlords',
+    'Have you ever eaten a hamburger?',
+    'Spain is nice in the autumn'
+  ];  
+
+  it('should return message', () => {
     const pendingPromise = api.botReply()
+        .then(resolved => {    
+          const { message } = resolved;
+          expect(responses).toContainEqual(message);
+        });
+
+    jest.runAllTimers();
+
+    api.botReply()
         .then(resolved => {
-            expect(resolved).toBe('something')
-        })
-    // Activate the timer (pretend the specified time has elapsed).
-    jest.runAllTimers()
-    // Return the promise, so Jest waits for its completion and fails the
-    // test when the promise is rejected.
-    return pendingPromise  
-  });  
+          const { message:newMessage } = resolved;
+          expect(responses).toContainEqual(newMessage);
+        });
+
+    jest.runAllTimers();
+
+    return pendingPromise;   
+
+  }); 
+
+  it('should store persona to localStorage', () => {
+    const persona = 'Zac';
+    api.storeCurrentPersona({'persona':persona});
+    expect(api.fetchCurrentPersona().persona).toBe(persona);
+  }); 
+
+  it('should return empty array of personas from localStorage', () => {
+    // console.log(api.fetchPersonas());
+    expect(api.fetchPersonas()).toHaveLength(0);
+  });   
 
 });
