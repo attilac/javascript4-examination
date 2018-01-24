8628cc27eb38d8bb9c74a227d0c59bb617b92711
attilac <attila.cederbygd@gmail.com>
2018-01-24 12:46:34 +0100

    started api src tests

--- src/__tests__/api/index.js
+++ src/__tests__/api/index.js
@@ -1,9 +1,24 @@
-import * as api from '../api';
+import * as api from '../../api';
 
 describe('api tests', ()=> {
+  // jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
   it('should return random message', () => {
     console.log('hej');
   });
+
+  jest.useFakeTimers();
+
+  it('works with resolves', () => {
+    const pendingPromise = api.botReply()
+        .then(resolved => {
+            expect(resolved).toBe('something')
+        })
+    // Activate the timer (pretend the specified time has elapsed).
+    jest.runAllTimers()
+    // Return the promise, so Jest waits for its completion and fails the
+    // test when the promise is rejected.
+    return pendingPromise  
+  });  
 
 });
