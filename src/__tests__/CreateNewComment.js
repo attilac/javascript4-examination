aab7154968f5ccadde86d7aecf8cb9f6dc6ff79e
attilac <attila.cederbygd@gmail.com>
2018-01-31 11:00:48 +0100

    divided a test to smaller blocks

--- src/__tests__/CreateNewComment.js
+++ src/__tests__/CreateNewComment.js
@@ -4,11 +4,19 @@ import CreateNewComment from '../components/CreateNewComment';
 
 describe('CreateNewComment unit tests', ()=> {
 
-  it('should store comment to localStorage and clear state after', () => {
+  it('should store comment to localStorage on submit', () => {
     const updateComments = jest.fn();
     const wrapper = mount(<CreateNewComment  author="Attila" postId="1" updateComments={updateComments} />);
     wrapper.setState({comment: 'testcomment'})
-    expect(wrapper.state().comment).toBe('testcomment');   
+    wrapper.find('form').simulate('submit');
+    const newComment = JSON.parse(localStorage.getItem('comments'))[0].comment;
+    expect(newComment).toBe('testcomment');
+  });
+
+  it('should clear state after submit', () => {
+    const updateComments = jest.fn();
+    const wrapper = mount(<CreateNewComment  author="Attila" postId="1" updateComments={updateComments} />);
+    wrapper.setState({comment: 'testcomment'})
     wrapper.find('form').simulate('submit');
     const newComment = JSON.parse(localStorage.getItem('comments'))[0].comment;
     expect(newComment).toBe('testcomment');
